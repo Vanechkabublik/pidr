1b72665bfa60fa0d3822487ff41a7fc86de33575
Your Name <leonarov@vk.com>
2025-09-04 00:21:11 +0400

    sdfs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,10 +12,7 @@ export class AuthController {
   @Get('vk/callback')
   @UseGuards(AuthGuard('vk')) // Используем тот же гард для обработки обратного вызова
   async callback(@Request() req, @Response() res) {
-    const user = req.user; // Профиль пользователя доступен тут
-    if (!user) {
-      return res.status(401).send({ error: 'Authentication failed' });
-    }
+    const user = req.profile; // Профиль пользователя доступен тут
 
     console.log(user); // Здесь будут видны полученные данные
     return res.send({ message: 'Successfully logged in!', data: user });
